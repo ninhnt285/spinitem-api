a3c726f190d65b0282a6f9dbc64d4a414d6e7a28
nick <salem280575@gmail.com>
2018-03-08 03:55:09 -0500

    Support multiple image size

--- models/uploadfile/uploadfile_model.go
+++ models/uploadfile/uploadfile_model.go
@@ -8,9 +8,24 @@ import (
 	"strconv"
 	"time"
 
+	"gopkg.in/h2non/bimg.v1"
+
 	"github.com/rs/xid"
 
 	"../../helpers/config"
+)
+
+var (
+	imgOptions = []bimg.Options{
+		// Square images
+		bimg.Options{Width: 50, Height: 50, Crop: true},
+		bimg.Options{Width: 100, Height: 100, Crop: true},
+		bimg.Options{Width: 150, Height: 150, Crop: true},
+		bimg.Options{Width: 500, Height: 500, Crop: true},
+		// Resize images
+		bimg.Options{Width: 500, Height: 500, Enlarge: true},
+		bimg.Options{Width: 750, Height: 750, Enlarge: true},
+		bimg.Options{Width: 1000, Height: 1000, Enlarge: true}}
 )
 
 // UploadFile save file info
@@ -37,14 +52,14 @@ func (uf *UploadFile) Add(r *http.Request, fileDir string) error {
 		strconv.Itoa(currentTime.Year()) + "/" +
 		strconv.Itoa(int(currentTime.Month())) + "/" +
 		strconv.Itoa(currentTime.Day()) + "/"
-	fileName := xid.New().String() + ext
+	fileName := xid.New().String()
 
 	uf.Title = handler.Filename
-	uf.Destination = destinationDir + fileName
+	uf.Destination = destinationDir + fileName + "%s" + ext
 	uf.Size = handler.Size
 	// Create directory tree
 	os.MkdirAll(cf.PublicDir+destinationDir, 0755)
-	f, err := os.OpenFile(cf.PublicDir+uf.Destination, os.O_WRONLY|os.O_CREATE, 0666)
+	f, err := os.OpenFile(cf.PublicDir+destinationDir+fileName+ext, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return err
 	}
@@ -54,5 +69,32 @@ func (uf *UploadFile) Add(r *http.Request, fileDir string) error {
 	if err != nil {
 		return err
 	}
+	// Resize images
+	resizeImages(cf.PublicDir+destinationDir, fileName, ext)
+
 	return nil
+}
+
+func resizeImages(dir string, filename string, ext string) {
+	for _, option := range imgOptions {
+		resizeImage(dir, filename, ext, option)
+	}
+}
+
+func resizeImage(dir string, filename string, ext string, option bimg.Options) error {
+	// Read Image
+	buffer, err := bimg.Read(dir + filename + ext)
+	if err != nil {
+		return err
+	}
+	// Generate new filename
+	suffix := "_" + strconv.Itoa(option.Width) + "x" + strconv.Itoa(option.Height)
+	if option.Crop {
+		suffix += "_square"
+	}
+	var newImageName = dir + filename + suffix + ext
+	// Resize Image
+	newImage, err := bimg.NewImage(buffer).Process(option)
+	bimg.Write(newImageName, newImage)
+	return err
 }
